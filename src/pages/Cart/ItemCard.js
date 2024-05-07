ce6c43e242b2567ce126bdf42db413c0d154e9e6
AnshumanShah69 <februaryleapyear29@gmail.com>
2024-05-07 13:57:10 +0530

    not happening

--- src/pages/Cart/ItemCard.js
+++ src/pages/Cart/ItemCard.js
@@ -20,7 +20,7 @@ const ItemCard = ({ item }) => {
         <h1 className="font-titleFont font-semibold">{item.name}</h1>
       </div>
       {/* made change here */}
-      <div className="col-span-5 mdl:col-span-3 py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
+      <div className="col-span-5 mdl:col-span-3 items-center justify-between py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
         <div className="flex w-1/3 items-center text-lg font-semibold">
           ${item.price}
         </div>
