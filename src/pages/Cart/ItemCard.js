2f45441c82afd6ad24f4d77c0d8adeae5287a6b0
AnshumanShah69 <februaryleapyear29@gmail.com>
2024-05-07 14:02:49 +0530

    end commit

--- src/pages/Cart/ItemCard.js
+++ src/pages/Cart/ItemCard.js
@@ -19,7 +19,7 @@ const ItemCard = ({ item }) => {
         <img className="w-32 h-32" src={item.image} alt="productImage" />
         <h1 className="font-titleFont font-semibold">{item.name}</h1>
       </div>
-      <div className="col-span-5 mdl:col-span-3 flex items-center justify-between py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
+      <div className="col-span-5 mdl:col-span-3 flex items-center justify-end py-4 mdl:py-0 px-4 mdl:px-0 gap-6 mdl:gap-0">
         <div className="flex w-1/3 items-center text-lg font-semibold">
           ${item.price}
         </div>
